27d5e12a906a234d444f20a9390deb2ba905297e
Freddie Haddad <freddie.haddad@outlook.com>
2025-05-23 21:21:41 -0700

    Initial release of ms2cc

---
+++ README.md
@@ -0,0 +1,155 @@
+# MS2CC - MSBuild to Compile Commands
+
+A Rust CLI tool that uses `msbuild.log` files to generate a compliant[^1]
+`compile_commands.json` for use with C/C++ language servers.
+
+## Introduction
+
+This tool enables full Language Server Protocol ([LSP]) support for any C/C++
+language server that relies on a `compile_commands.json` database by converting
+the `msbuild.log` output generated during a full MSBuild project build. Two such
+language servers are [clangd] and [Microsoft C/C++ Extension] for Visual Studio
+Code.
+
+The `compile_commands.json` file is a database that lists all source files in
+your project along with the exact commands needed to compile each one. Each
+entry follows this structure:
+
+```json
+[
+    {
+        "file": "main.cpp",
+        "directory": "C:\\projects\\example",
+        "arguments": [
+            "cl.exe", "/EHsc", "/Zi", "/D", "DEBUG", "main.cpp"
+        ]
+    }
+]
+```
+
+## Installation
+
+There are two ways to obtain the `ms2cc.exe` binary: download a precompiled
+release or build it from source.
+
+### Building from Source
+
+Since the tool was written in [Rust], the Rust development toolchain is
+required.
+
+1. Prepare your environment by following the instructions on the
+   [Rust installation page].
+1. Clone the repository.
+1. Build a debug release with `cargo build` or a release version with
+   `cargo build --release`.
+1. The generated executable will be under the `./target/debug/` or
+   `./target/release` directory.
+
+## Generating the Database
+
+1. Complete a full build of your C/C++ MSBuild project.
+
+1. Run `ms2cc.exe`:
+
+   ```console
+   ms2cc.exe --input-file <PATH_TO_MSBUILD.LOG> --source-directory <PATH_TO_PROJECT_SOURCE>
+   ```
+
+   This will create a `compile_commands.json` file in the same directory where
+   you ran `ms2cc.exe`.
+
+   You can use the `--output-file` option to specify the name and location for
+   the database to be written.
+
+> **NOTE:** You can run `ms2cc.exe` with the `--help` option for more details.
+
+```console
+$ ms2cc.exe --help
+Tool to generate a compile_commands.json file from an msbuild.log file.
+
+Usage: ms2cc.exe [OPTIONS] --input-file <INPUT_FILE> --source-directory <SOURCE_DIRECTORY>
+
+Options:
+  -i, --input-file <INPUT_FILE>              Path to msbuild.log
+  -o, --output-file <OUTPUT_FILE>            Output JSON file [default: compile_commands.json]
+  -d, --source-directory <SOURCE_DIRECTORY>  Path to source code
+  -c, --compiler-executable <EXE>            Name of compiler executable [default: cl.exe]
+  -h, --help                                 Print help
+  -V, --version                              Print version
+```
+
+## Editor Configuration
+
+Configuring your editor to make use of the `compile_commands.json` database
+requires a few steps:
+
+1. Install a C/C++ language server
+1. Configure your editor
+1. Point the language server to your project
+
+### Visual Studio Code
+
+In VSCode, you can install the [Microsoft C/C++ Extension] to enable C/C++
+language support. Once installed, a `.vscode` directory will be created in your
+project folder, containing a `c_cpp_properties.json` file. The `configurations`
+key in this file holds an array of project-specific settings. For each relevant
+configuration, add the `compileCommands` property as shown below:
+
+```json
+{
+    "configurations": [
+        {
+            "compileCommands": "${workspaceFolder}/.vscode/compile_commands.json"
+        }
+    ]
+}
+```
+
+> **NOTE:** Set the path to the location of your `compile_commands.json` file.
+
+### Other Editors
+
+For most editors, [clangd] is likely the best choice. It automatically searches
+for a `compile_commands.json` file by traversing up the directory tree from the
+file you're editing, as well as checking any `build/` subdirectories. For
+instance, if you're editing `$SRC/gui/window.cpp,` clangd will look in
+`$SRC/gui/`, `$SRC/gui/build/`, `$SRC/`, `$SRC/build/`, and so on. You can also
+explicitly specify the path to the compilation database in your clangd
+configuration file:
+
+```yaml
+CompileFlags:
+  CompilationDatabase: c:\\path\\to\\your\\compile_commands.json
+```
+
+> **NOTE:** See the [clangd configuration] documentation for details.
+
+## Known Issues
+
+**Handling Duplicate Source File Names with Relative Paths**
+
+In some cases, multiple source files in a project may share the same name, and
+the associated compile commands might not include absolute file paths. Since
+absolute paths are required, the tool attempts to disambiguate these entries
+using the `/Fo` compiler flag (if present in the compile command), which
+specifies the output object file. However, if the object file path lies outside
+the directory of the corresponding source file, this method may fail. When that
+happens, an error message is logged.
+
+## Contributing
+
+Contributions are welcome. Simply open a PR!
+
+## Contact
+
+For questions or comments, please start a [discussion on github].
+
+[^1]: https://clang.llvm.org/docs/JSONCompilationDatabase.html
+
+[clangd]: https://clangd.llvm.org/
+[clangd configuration]: https://clangd.llvm.org/config
+[discussion on github]: https://github.com/fhaddad_microsoft/ms2cc/discussions
+[lsp]: https://microsoft.github.io/language-server-protocol/
+[microsoft c/c++ extension]: https://code.visualstudio.com/docs/languages/cpp
+[rust]: https://www.rust-lang.org/
+[rust installation page]: https://www.rust-lang.org/tools/install
---
+++ src/main.rs
@@ -0,0 +1,406 @@
+use anyhow::{Context, Result, ensure};
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::{File, read_dir};
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+use std::sync::mpsc::{Receiver, Sender, channel};
+use std::thread;
+
+/// compile_commands.json entry descriptor
+#[derive(Deserialize, Serialize)]
+struct CompileCommand {
+    file: PathBuf,
+    directory: PathBuf,
+    arguments: Vec<String>,
+}
+
+/// Command line arguments
+#[derive(Parser)]
+#[command(
+    version,
+    about = "Tool to generate a compile_commands.json file from an msbuild.log file."
+)]
+struct Cli {
+    /// Path to msbuild.log
+    #[arg(short('i'), long)]
+    input_file: PathBuf,
+
+    /// Output JSON file
+    #[arg(short('o'), long, default_value = "compile_commands.json")]
+    output_file: PathBuf,
+
+    /// Path to source code
+    #[arg(short('d'), long)]
+    source_directory: PathBuf,
+
+    /// Name of compiler executable
+    #[arg(short('c'), long, name = "EXE", default_value = "cl.exe")]
+    compiler_executable: String,
+}
+
+/// Error handler.  Reports any received errors to `STDERR`.
+fn error_handler(error_rx: Receiver<String>) {
+    while let Ok(e) = error_rx.recv() {
+        eprintln!("{e}");
+    }
+}
+
+/// Explores the directory tree `path`, visiting all directories, and sending
+/// any files found on the `entry_tx` sender channel. Any IO errors are reported
+/// to the `error_tx` channel.
+fn find_all_files(
+    path: PathBuf,
+    entry_tx: Sender<PathBuf>,
+    error_tx: Sender<String>,
+) {
+    let mut stack = vec![path];
+    while let Some(path) = stack.pop() {
+        let reader = match read_dir(&path) {
+            Ok(r) => r,
+            Err(e) => {
+                let e = format!("read_dir error for {path:?}: {e}");
+                let _ = error_tx.send(e);
+                continue;
+            }
+        };
+        for entry in reader {
+            let entry = match entry {
+                Ok(de) => de,
+                Err(e) => {
+                    let e = format!("Failed to read from {path:?}: {e}",);
+                    let _ = error_tx.send(e);
+                    continue;
+                }
+            };
+
+            let path = entry.path();
+            if path.is_dir() {
+                stack.push(path);
+                continue;
+            }
+
+            if !path.is_file() {
+                let e = format!("{path:?} is not a file");
+                let _ = error_tx.send(e);
+                continue;
+            }
+
+            // Normalize the path
+            if let Some(path) =
+                path.to_str().map(|s| s.to_lowercase()).map(PathBuf::from)
+            {
+                let _ = entry_tx.send(path);
+            } else {
+                let e = format!("Failed to normalize {path:?}");
+                let _ = error_tx.send(e);
+                continue;
+            }
+        }
+    }
+}
+
+/// Generates a hash map of file/path entries from all files sent to the
+/// `entry_rx` channel.  This map is used for compile command entries found in
+/// `msbuild.log` that only include a file name without a path.
+///
+/// NOTE: All paths are expected to be lowercase.
+fn build_file_map(entry_rx: Receiver<PathBuf>) -> HashMap<PathBuf, PathBuf> {
+    // Generate a map of files and their directories
+    let mut tree = HashMap::new();
+    while let Ok(path) = entry_rx.recv() {
+        // Test if entry is a file with an extension
+        if path.extension().is_some() {
+            let file_name = PathBuf::from(path.file_name().unwrap());
+            let parent = PathBuf::from(path.parent().unwrap());
+
+            // Add KV pair (file/path) to the hash table; clear on collision
+            tree.entry(file_name)
+                .and_modify(|absolute_path: &mut PathBuf| {
+                    *absolute_path = PathBuf::new()
+                })
+                .or_insert(parent);
+        }
+    }
+    tree
+}
+
+/// Searches an `msbuild.log` for all lines containing `s` string and sends
+/// them out on the `tx` channel.
+fn find_all_lines(reader: BufReader<File>, s: &str, tx: Sender<String>) {
+    reader.lines().map_while(Result::ok).for_each(|line| {
+        if line.to_lowercase().contains(s) {
+            let _ = tx.send(line);
+        }
+    });
+}
+
+/// Listens on the `rx` channel for strings and strips them of all superfluous
+/// characters.  Sends the updated string on the `tx` channel.
+fn cleanup_line(rx: Receiver<String>, tx: Sender<String>) {
+    while let Ok(s) = rx.recv() {
+        let s = s.replace("\"", "");
+        let _ = tx.send(s);
+    }
+}
+
+/// Converts strings received on the `rx` channel into tokens and sends them out
+/// on the `tx` channel.
+fn tokenize_lines(rx: Receiver<String>, tx: Sender<Vec<String>>) {
+    while let Ok(s) = rx.recv() {
+        let t: Vec<_> = s.split_whitespace().map(String::from).collect();
+        let _ = tx.send(t);
+    }
+}
+
+/// When a compile command in the log file uses an absolute path to the source file, all required
+/// components exist to generate a `CompileCommand`.
+fn create_compile_command(
+    path: PathBuf,
+    arguments: Vec<String>,
+    error_tx: Sender<String>,
+) -> Option<CompileCommand> {
+    let directory = match path.parent() {
+        Some(parent) => PathBuf::from(parent),
+        None => {
+            let e = format!("Missing parent component in {:?}", path);
+            let _ = error_tx.send(e);
+            return None;
+        }
+    };
+
+    let file = match path.file_name() {
+        Some(file_name) => PathBuf::from(file_name),
+        None => {
+            let e = format!("Missing file_name component in {:?}", path);
+            let _ = error_tx.send(e);
+            return None;
+        }
+    };
+
+    Some(CompileCommand {
+        file,
+        directory,
+        arguments,
+    })
+}
+
+/// Converts a stream of tokens received on the `rx` channel into a
+/// `CompileCommand` and sends it out on the `tx` channel. The `map` generated
+/// by `build_file_map` is used to find the paths to any source files that did
+/// not include it in `msbuild.log`. Errors are reported on the `error_tx`
+/// channel
+fn create_compile_commands(
+    map: HashMap<PathBuf, PathBuf>,
+    rx: Receiver<Vec<String>>,
+    tx: Sender<CompileCommand>,
+    error_tx: Sender<String>,
+) {
+    while let Ok(arguments) = rx.recv() {
+        // The file name should be the last compiler argument
+        let mut path = match arguments.last() {
+            Some(path) => PathBuf::from(path.to_lowercase()),
+            None => {
+                let e = String::from("Token vector is empty!");
+                let _ = error_tx.send(e);
+                continue;
+            }
+        };
+
+        // Is the last argument in the compile command a file?
+        let file_name = match path.file_name() {
+            Some(file_name) => PathBuf::from(file_name),
+            None => {
+                let e = format!("Missing file_name component in {path:?}");
+                let _ = error_tx.send(e);
+                continue;
+            }
+        };
+
+        // Does it have an extension?
+        if file_name.extension().is_none() {
+            let e = format!("File name missing extension {file_name:?}");
+            let _ = error_tx.send(e);
+            continue;
+        }
+
+        // If we only have a file name or relative path, try to reconstruct an
+        // absolute path.
+
+        // First we check our directory tree
+        if !path.is_absolute() {
+            if let Some(parent) = map.get(&file_name) {
+                path = parent.clone();
+                path.push(&file_name);
+            };
+        }
+
+        // Last option is to try and reconstruct the path from the /Fo argument.
+        if !path.is_absolute() {
+            let argument = "/Fo";
+            if let Some(fo_argument) =
+                arguments.iter().find(|s| s.starts_with(argument))
+            {
+                path = PathBuf::from(
+                    fo_argument.strip_prefix(argument).unwrap().to_lowercase(),
+                );
+
+                while path.has_root() {
+                    path.push(&file_name);
+
+                    // Did we find the path?
+                    if path.is_file() {
+                        break;
+                    }
+                    path.pop();
+
+                    // Reached the end?
+                    if !path.pop() {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if !path.is_absolute() || !path.is_file() {
+            let e =
+                format!("Failed to retreive an absolute path to {file_name:?}");
+            let _ = error_tx.send(e);
+            continue;
+        }
+
+        // Found the path
+        if let Some(cc) =
+            create_compile_command(path, arguments, error_tx.clone())
+        {
+            let _ = tx.send(cc);
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    // Parse command line arguments
+    let cli = Cli::parse();
+
+    let package_name = env!("CARGO_PKG_NAME");
+    let package_version = env!("CARGO_PKG_VERSION");
+    println!("{package_name} v{package_version}");
+
+    // File reader
+    let input_file_handle = BufReader::new(
+        File::open(&cli.input_file)
+            .with_context(|| format!("Failed to open {:?}", cli.input_file))?,
+    );
+
+    // Verify source directory is a valid path
+    ensure!(
+        cli.source_directory.is_dir(),
+        format!(
+            "Provided path is not a directory: {:?}",
+            cli.source_directory
+        )
+    );
+
+    // File writer
+    let output_file_handle = File::options()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(&cli.output_file)
+        .with_context(|| format!("Failed to open {:?}", cli.output_file))?;
+
+    println!(
+        "Preparing to generate the lookup tree (this will take some time) ..."
+    );
+    let tree = thread::scope(|s| {
+        let (entry_tx, entry_rx) = channel();
+        let (error_tx, error_rx) = channel();
+
+        // Separate thread for error handling.
+        s.spawn(move || {
+            println!("Error handling thread initialized.");
+            error_handler(error_rx);
+        });
+
+        // Process discovered files
+        let h = s.spawn(move || {
+            println!("Tree generating thread initilized.");
+            // Value is returned by the thread
+            build_file_map(entry_rx)
+        });
+
+        // Traverse the directory tree
+        println!("Directory thraversal thread initialized.");
+        find_all_files(cli.source_directory, entry_tx, error_tx);
+
+        // Return the tree to the main thread
+        h.join().unwrap()
+    });
+    println!("Finished");
+
+    println!(
+        "Preparing to generate {:?} (this will take some time) ...",
+        cli.output_file
+    );
+    thread::scope(|s| {
+        let (source_tx, source_rx) = channel();
+        let (preprocess_tx, preprocess_rx) = channel();
+        let (token_tx, token_rx) = channel();
+        let (compile_command_tx, compile_command_rx) = channel();
+        let (error_tx, error_rx) = channel();
+
+        // Separate thread for error handling.
+        s.spawn(move || {
+            println!("Error handling thread initialized.");
+            error_handler(error_rx);
+        });
+
+        // Collect all the compile commands from the input file
+        s.spawn(move || {
+            println!("Log searching thread initialized.");
+            println!(
+                "Scanning {:?} (this will take some time) ...",
+                cli.input_file
+            );
+            find_all_lines(
+                input_file_handle,
+                &cli.compiler_executable,
+                source_tx,
+            );
+        });
+
+        // Remove nested quotes (")
+        s.spawn(move || {
+            println!("Log entry cleanup thread initialized.");
+            cleanup_line(source_rx, preprocess_tx);
+        });
+
+        // Tokenize
+        s.spawn(move || {
+            println!("Log entry tokenization thread initialized.");
+            tokenize_lines(preprocess_rx, token_tx);
+        });
+
+        // Verify the input
+        s.spawn(move || {
+            println!("Compile command generation thread initialized.");
+            create_compile_commands(
+                tree,
+                token_rx,
+                compile_command_tx,
+                error_tx,
+            );
+        });
+
+        // Generate the compile_commands.json file
+        println!("Waiting for compile commands ...",);
+        let compile_commands: Vec<_> = compile_command_rx.iter().collect();
+        println!("Writing {:?} database ...", cli.output_file);
+        let _ =
+            serde_json::to_writer_pretty(output_file_handle, &compile_commands);
+    });
+    println!("Finished");
+
+    Ok(())
+}
